2d6de5d95252f237141514a1621c8a7a824aa11a
Sunny <sunnyplay333@gmail.com>
2026-02-24 13:51:45 -0600

    Bot limpio sin token

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+// 🔴 PEGA AQUÍ TU TOKEN
+const TOKEN = process.env.TOKEN;
+
+// 🔵 PON AQUÍ EL ID DEL CANAL WELCOME
+const WELCOME_CHANNEL_ID = "PE1475909897265877103";
+
+const frases = [
+    "¡Miren quién llegó",
+    "Ya está aquí",
+    "Llegó a la fiesta",
+    "Nos alegra que hayas venido",
+    "¡Qué bueno verte"
+];
+
+client.on("guildMemberAdd", (member) => {
+
+    const canal = member.guild.channels.cache.get(WELCOME_CHANNEL_ID);
+    if (!canal) return;
+
+    const fraseRandom = frases[Math.floor(Math.random() * frases.length)];
+
+    canal.send(`➡️ ${fraseRandom}, ${member}!`);
+});
+
+client.once("clientReady", () => {
+    console.log(`Bot listo como ${client.user.tag}`);
+});
+
+client.login(TOKEN);
