b3b1ff0105ac252bc46870d4e14bfd0405f64e84
Sunny <sunnyplay333@gmail.com>
2026-02-24 16:41:14 -0600

    Aru reacciona a canciones

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
     
